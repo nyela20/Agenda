cb39641957ad6119b1d3570079b8d065e4da89d9
GamersOD <valeriangeorges@gmail.com>
2024-10-17 15:32:04 +0200

    push

--- Agenda/routes/rendezvous.js
+++ Agenda/routes/rendezvous.js
@@ -11,7 +11,20 @@ router.get('/:agendaId/creer', function(req, res) {
     res.render('creerRendezVous', { title: 'Création de Rendez-vous', agendaId, userEmailConnected: localStorage.getItem("userEmail") });
 });
 
-// création d'un nouveau rendez-vous
+// affiche les paramètres d'un rendez-vous
+router.get('/:agendaId/informations/:rendezvousId', async function(req, res, next) {
+    const agendaId = req.params.agendaId; // récupérer l'ID de l'agenda
+    const rdv = req.params.rendezvousId; // récupérer l'ID de du rendez-vous
+    res.render('RendezVousInfos', { title: 'Informations sur le Rendez-vous', rdv, agendaId, userEmailConnected: localStorage.getItem("userEmail") });
+});
+
+// suppression d'un rendez-vous
+router.post('/:agendaId/supprimer/:rdvId', async function(req, res, next) {
+    await rendezVousController.supprimerRendezVous(req, res, next);
+});
+
+
+// création d'un nouveau rendez-vouS
 router.post('/:agendaId/creer', async function(req, res, next) {
     await rendezVousController.creerRendezVous(req, res, next);
 });
